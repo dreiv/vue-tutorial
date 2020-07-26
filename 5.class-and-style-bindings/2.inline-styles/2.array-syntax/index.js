43765d7b99d1eb1937d4fdf5d43ad2c0a2accb41
Andrei Voicu <dreivoicu@gmail.com>
2020-07-26 16:06:40 +0300

    style bindings

---
+++ 5.class-and-style-bindings/2.inline-styles/2.array-syntax/index.js
@@ -0,0 +1,12 @@
+var app = new Vue({
+  el: '#app',
+  data: {
+    baseStyles: {
+      color: 'orange',
+      fontSize: '24px'
+    },
+    overridingStyles: {
+      fontWeight: 'bold'
+    }
+  }
+});
