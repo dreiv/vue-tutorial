5f63ea44fe1fdd503986a7de92d5abc82bb3ddb0
Andrei Voicu <dreivoicu@gmail.com>
2020-07-28 20:52:26 +0300

    single root

---
+++ 10.components/3.single-root-el/index.js
@@ -0,0 +1,28 @@
+Vue.component('blog-post', {
+  props: ['post'],
+  template: `
+    <div class="blog-post">
+      <h3>{{ post.title }}</h3>
+      <div v-html="post.content"></div>
+    </div>
+  `
+});
+
+var app = new Vue({
+  el: '#app',
+  data: {
+    posts: [
+      { id: 1, title: 'My journey with Vue', content: 'let your memory dance' },
+      {
+        id: 2,
+        title: 'Blogging with Vue',
+        content: 'why are you so afraid of living'
+      },
+      {
+        id: 3,
+        title: 'Why Vue is so fun',
+        content: 'we have two selves'
+      }
+    ]
+  }
+});
