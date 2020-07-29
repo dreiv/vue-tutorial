c23c93843778af4eb404e2733dfcf2c9d7d894fc
Andrei Voicu <dreivoicu@gmail.com>
2020-07-29 19:28:33 +0300

    slot

---
+++ 10.components/6.slots/index.js
@@ -0,0 +1,13 @@
+Vue.component('alert-box', {
+  template: `
+    <div class="demo-alert-box">
+      <strong>Error!</strong>
+      <slot></slot>
+    </div>
+  `
+});
+
+var app = new Vue({
+  el: '#app',
+  data: {}
+});
