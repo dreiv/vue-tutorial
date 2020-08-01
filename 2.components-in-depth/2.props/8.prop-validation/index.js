6ccedb8fef1a24c859fcf6f97b3e35d62dc326a4
Andrei Voicu <dreivoicu@gmail.com>
2020-08-01 19:45:11 +0300

    prop validation

---
+++ 2.components-in-depth/2.props/8.prop-validation/index.js
@@ -0,0 +1,40 @@
+Vue.component('my-component', {
+  props: {
+    // Basic type check (`null` and `undefined` values will pass any type validation)
+    propA: Number,
+    // Multiple possible types
+    propB: [String, Number],
+    // Required string
+    propC: {
+      type: String,
+      required: true
+    },
+    // Number with a default value
+    propD: {
+      type: Number,
+      default: 100
+    },
+    // Object with a default value
+    propE: {
+      type: Object,
+      // Object or array defaults must be returned from
+      // a factory function
+      default() {
+        return { message: 'hello' };
+      }
+    },
+    // Custom validator function
+    propF: {
+      validator(value) {
+        // The value must match one of these strings
+        return ['success', 'warning', 'danger'].indexOf(value) !== -1;
+      }
+    }
+  },
+  template: '<p>dummy component</p>'
+});
+
+var app = new Vue({
+  el: '#app',
+  data: {}
+});
