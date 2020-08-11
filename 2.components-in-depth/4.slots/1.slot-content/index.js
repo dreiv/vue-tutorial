f2e913d7699131a4d16aea17ec80b746c2e94193
Andrei Voicu <dreivoicu@gmail.com>
2020-08-11 18:59:37 +0300

    basic slot

---
+++ 2.components-in-depth/4.slots/1.slot-content/index.js
@@ -0,0 +1,17 @@
+Vue.component('navigation-link', {
+  props: {
+    url: String
+  },
+  template: `
+    <a
+      :href="url"
+      class="nav-link"
+    >
+      <slot></slot>
+    </a>
+  `
+});
+
+var app = new Vue({
+  el: '#app'
+});
