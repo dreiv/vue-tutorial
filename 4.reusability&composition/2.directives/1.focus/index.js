d5d80c629382c1e99c2ad1ca06ff4b69d9ce0b18
Andrei Voicu <dreivoicu@gmail.com>
2020-08-14 22:11:44 +0300

    global directive

---
+++ 4.reusability&composition/2.directives/1.focus/index.js
@@ -0,0 +1,12 @@
+Vue.directive('focus', {
+  // When the bound element is inserted into the DOM...
+  inserted: function (el) {
+    // Focus the element
+    el.focus();
+  }
+});
+
+var app = new Vue({
+  el: '#app',
+  data: {}
+});
