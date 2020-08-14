ab614144fa2305e12746188559680dcebb47723c
Andrei Voicu <dreivoicu@gmail.com>
2020-08-14 16:15:14 +0300

    list transitions

---
+++ 3.transitions&animations/7.list-transitions/index.js
@@ -0,0 +1,21 @@
+var app = new Vue({
+  el: '#app',
+  data: {
+    items: [1, 2, 3, 4, 5, 6, 7, 8, 9],
+    nextNum: 10
+  },
+  methods: {
+    randomIndex() {
+      return Math.floor(Math.random() * this.items.length);
+    },
+    add() {
+      this.items.splice(this.randomIndex(), 0, this.nextNum++);
+    },
+    remove() {
+      this.items.splice(this.randomIndex(), 1);
+    },
+    shuffle() {
+      this.items = _.shuffle(this.items);
+    }
+  }
+});
